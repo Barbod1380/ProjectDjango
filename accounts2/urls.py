96378fd5b74304cd5f788460e383a212424e752b
Barbod1380 <barboad1380@gmail.com>
2021-03-02 17:19:41 -0800

    PHASE2

--- accounts2/urls.py
+++ accounts2/urls.py
@@ -4,13 +4,9 @@ from . import views
 
 urlpatterns = [
 
-    path('polls/Teacher_register/', views.TregisterPage),
-    path('polls/Student_register/', views.SregisterPage),
-    path('polls/Teacher_register/polls/Teacher_Login/', views.TLoginPage),
-    path('polls/Student_register/polls/Student_Login/', views.SLoginPage),
-    path('polls/Teacher_register/polls/Teacher_Login/polls/Teacher_Home', views.THomePage),
-    path('polls/Student_register/polls/Student_Login/polls/Student_Home', views.SHomePage),
-    path('polls/', views.WhoAreYou)
+    path('polls/polls/Login/', views.LoginPage),
+    path('polls/polls/Login/polls/Home', views.HomePage),
+    path('polls/', views.registerPage)
 
 ]
 
--- accounts2/views.py
+++ accounts2/views.py
@@ -7,23 +7,8 @@ from .models import Video
 
 
 
-
-def WhoAreYou(request):
-    context = {}
-    return render(request, 'Howiat.html', context)
-def TregisterPage(request):
+def registerPage(request):
     form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('polls/Teacher_Login')
-
-    context = {'form': form}
-    return render(request, 'accounts/Teacher_register.html', context)
-def SregisterPage(request):
-    form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
@@ -31,8 +16,11 @@ def SregisterPage(request):
             return redirect('polls/Student_Login')
 
     context = {'form': form}
-    return render(request, 'accounts/Student_register.html', context)
-def TLoginPage(request):
+    return render(request, 'accounts/register.html', context)
+
+
+
+def LoginPage(request):
     form = CreateUserForm()
     if request.method == 'POST':
         password = request.POST.get('password')
@@ -40,27 +28,14 @@ def TLoginPage(request):
         user = authenticate(request, password=password, email=email)
         if user is None:
              login(request, user)
-             return redirect('polls/Teacher_Home')
+             return redirect('polls/Home')
 
 
     context = {'form':form}
-    return render(request, 'accounts/Teacher_login.html', context)
-def SLoginPage(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        user = authenticate(request, password=password, email=email)
-
-        if user is None:
-            login(request, user)
-            return redirect('polls/Student_Home')
-
-    context = {'form': form}
-    return render(request, 'accounts/Student_login.html', context)
+    return render(request, 'accounts/login.html', context)
 
 
-def THomePage(request):
+def HomePage(request):
     all_video = Video.objects.all()
     if request.method == 'POST':
         form = Video_form(data=request.POST, files=request.FILES)
@@ -70,14 +45,6 @@ def THomePage(request):
     else:
         form=Video_form
     return render(request, 'accounts/index.html', {"form":form})
-
-
-def SHomePage(request):
-    return HttpResponse('Welcome our dear Student')
-
-
-
-
 
 
 
